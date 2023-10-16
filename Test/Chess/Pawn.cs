e9d1e42527c3c178164066656dee9a4526016c8a
Danil.Karpov <Danil.Karpov@1220-01>
2023-10-16 10:07:41 +0300

    Добавьте файлы проекта.

---
+++ Test/Chess/Pawn.cs
@@ -0,0 +1,16 @@
+﻿namespace Test.Chess
+{
+    public class King
+    {   //я написал вам подсказок если вы смотрите на доску
+        public int x;
+        public int y;
+
+        public bool Move(int newX, int newY)
+        {
+             return (Math.Abs(x - newX) <= 1 && Math.Abs(y - newY) <= 1);
+        }     
+        // x - start
+        // newX - new position
+        // x - newX <= 1
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,42 @@
+﻿Cat Cat = new Cat("red");
+Cat.color = "gray";
+Console.WriteLine(Cat.color);
+Cat.Meow();
+Cat.Eat();
+
+Tiger Tiger = new Tiger("red");
+Tiger.Meow();
+Tiger.Eat();
+Tiger.Hunting();
+class Cat
+{
+    public string color;
+
+    public Cat(string color)
+    {
+        this.color = color;
+        Console.WriteLine("Cat created!");
+    }
+
+    public void Meow()
+    {
+        Console.WriteLine("meow!");
+    }
+
+    public void Eat()
+    {
+        Console.WriteLine("eat!");
+    }
+}
+
+class Tiger : Cat {
+    public Tiger(string color) : base(color)
+    {
+        Console.WriteLine("Tiger created!");
+    }
+
+    public void Hunting()
+    {
+        Console.WriteLine("Start hunting!");
+    }
+}
